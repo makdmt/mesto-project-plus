5a8ac4c6ffafcbefc74a74a4f146891cd360725c
Dmitry Makarov <makdmt@gmail.com>
2024-02-28 11:21:25 +0300

    feat(users) add patch user info and user avatar routers and controllers

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,26 +1,43 @@
 import { Request, Response } from 'express';
 import User from '../models/user';
+import { STATUS_CODES } from '../helpers/constants';
 
 export const getUsers = (req: Request, res: Response) => {
- return User.find({})
-.then(user => res.send(user))
-.catch(err => res.status(404).send(err))
+  return User.find({})
+    .then(user => res.send(user))
+    .catch(err => res.status(404).send(err))
 }
 
 export const getUserById = (req: Request, res: Response) => {
   const { id } = req.params;
-  return User.find({id}).orFail(new Error('пользователь не найден'))
+  return User.find({ id }).orFail(new Error('пользователь не найден'))
 
-.then(user => res.send(user))
-.catch(err => res.status(404).send(err))
+    .then(user => res.send(user))
+    .catch(err => res.status(404).send(err))
 }
 
 export const createUser = (req: Request, res: Response) => {
   User.create({
- name: req.body.name,
- about: req.body.about,
- avatar: req.body.avatar
-})
-.then(user => res.send(user))
-.catch(err => res.status(400).send(err))
+    name: req.body.name,
+    about: req.body.about,
+    avatar: req.body.avatar
+  })
+    .then(user => res.send(user))
+    .catch(err => res.status(400).send(err))
+}
+
+export const patchUser = (req: Request, res: Response) => {
+  const { _id } = req.user;
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(_id, { name, about }, { new: true, runValidators: true })
+    .then(user => res.send({ user }))
+    .catch(err => res.status(STATUS_CODES.INTERNAL_SERVER_ERROR.statusCode).send({ message: STATUS_CODES.INTERNAL_SERVER_ERROR.message }))
+}
+
+export const patchUserAvatar = (req: Request, res: Response) => {
+  const { _id } = req.user;
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(_id, { avatar }, { new: true, runValidators: true })
+    .then(user => res.send({ user }))
+    .catch(err => res.status(STATUS_CODES.INTERNAL_SERVER_ERROR.statusCode).send({ message: STATUS_CODES.INTERNAL_SERVER_ERROR.message }))
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getUsers, getUserById, createUser } from "../controllers/users";
+import { getUsers, getUserById, createUser, patchUserAvatar, patchUser } from "../controllers/users";
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 userRouter.get('/:id', getUserById);
 userRouter.post('/', createUser);
+userRouter.patch('/me/avatar', patchUserAvatar);
+userRouter.patch('/me', patchUser);
 
 export default userRouter;
