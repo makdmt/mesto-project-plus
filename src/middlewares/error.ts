0fce6529f9b5e2469d506b7a6eca5a4467ad7516
Dmitry Makarov <makdmt@gmail.com>
2024-03-02 20:19:44 +0300

    feat(middlewares) add Validation errors handler to errMiddleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,9 +1,17 @@
-import { NextFunction, Request, Response } from "express";
+import { Errback, ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/errors";
 import { STATUS_CODES } from "../errors/status-codes";
+import mongoose, { Error } from "mongoose";
 
 export const errHandleMiddleware = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
   let { statusCode, message } = err;
+
+  console.log(err instanceof mongoose.Error.ValidationError);
+
+  if (err instanceof mongoose.Error.ValidationError) {
+    statusCode = STATUS_CODES.BAD_REQUEST.statusCode;
+  }
+
   if (!statusCode) {
     statusCode = STATUS_CODES.INTERNAL_SERVER_ERROR.statusCode
     message = STATUS_CODES.INTERNAL_SERVER_ERROR.message;
